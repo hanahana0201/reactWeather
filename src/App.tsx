f819bd2f1410d8a518a260214d78074399df234a
cocopon <cocopon@me.com>
2022-07-28 07:13:18 +0000

    Finish 「Results.tsxを作ろう　その1」

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Title from "./components/Title";
+import Form from "./components/Form";
+import Results from "./components/Results";
+import "./App.css";
 
 function App() {
+	const [city, setCity] = useState<string>("");
+	const getWeather = (e: any) => {
+		e.preventDefault();
+		fetch("http://api.weatherapi.com/v1/current.json?key=b91e5144d63e413ba8885703222307&q=London&aqi=no")
+			.then(res => res.json())
+			.then(data => console.log(data))
+	}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="test">
+			<Title />
+			<Form setCity={setCity} getWeather={getWeather} />
+      <Results/ >
     </div>
   );
 }
